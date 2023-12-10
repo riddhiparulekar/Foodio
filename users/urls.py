6be39d446f991b44f630357b3d88e977c00218e3
Riddhi Parulekar <riddhiparulekar2208@gmail.com>
2023-12-10 21:44:08 +0000

    loginsignup

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.home, name='home' ),
     path('aboutus',views.about,name='about'),
     path('login',views.login,name='login'),
-    path('logout',views.logout,name='logout')
+    path('logout',views.logout,name='logout'),
+    path('signup',views.signup,name='signup'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import auth,messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -26,3 +27,27 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect('home')
+
+
+def signup(request):
+    print('signup')
+    if request.method =="POST":
+        print('post')
+        username=request.POST.get('username')
+        first_name=request.POST.get('first_name')
+        last_name=request.POST.get('last_name')
+        emailid=request.POST.get('emailid')
+        password=request.POST.get('password2')
+
+        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, email=emailid, password=password)
+        user.save()
+        print('saved')
+        user=auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            print('logged in')
+            return redirect('home')
+        else:
+            messages.error(request,'Invalid login details')
+    return redirect('login')
