d7a065a143837136d3a59ba295213b999531b43f
Riddhi Parulekar <riddhiparulekar2208@gmail.com>
2023-12-11 23:45:14 +0000

    key vulnerability

--- foodio/settings.py
+++ foodio/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
-# SECRET_KEY = "django-insecure-yzvh(fdgp@z*%6*3rjj4%$-(@%b1+nmvk%^1r&(geo$0v1jmyn"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
